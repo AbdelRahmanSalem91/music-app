42a3e45be8e1e469372693d5688480ece198a7a1
AbdelRahmanSalem91 <abdoahmed424@gmail.com>
2022-02-17 00:36:54 +0200

    init and using vuex, vee-validate, rules and tailwind

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  state: {
+    authModalShow: false,
+  },
+  mutations: {
+    toggleAuthModal(state) {
+      state.authModalShow = !state.authModalShow;
+    },
+  },
+  actions: {
+  },
+  getters: {
+  },
+});
